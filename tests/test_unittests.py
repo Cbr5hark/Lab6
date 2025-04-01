7f2a2a2631fced678002b6bf0c5700057059e1a3
Matvey <matvey.m.920@gmail.com>
2025-04-01 19:31:15 +0300

    Update ci and tests

--- tests/test_unittests.py
+++ tests/test_unittests.py
@@ -5,7 +5,7 @@ from unittest.mock import Mock, patch
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from MyProject import TicTacToe
+from src.MyProject import TicTacToe
 
 class TestTicTacToe(unittest.TestCase):
     def setUp(self):
